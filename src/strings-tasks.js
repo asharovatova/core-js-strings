e7112fc1c8babee0968402f451b2902e4c1552e8
asharovatova <co.rin@mail.ru>
2024-11-30 02:07:14 +0300

    feat: implement getFirstChar

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -71,8 +71,12 @@ function concatenateStrings(value1, value2) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  if (typeof value === 'string' && value.length > 0) {
+    return value.charAt(0);
+  }
+
+  return '';
 }
 
 /**
