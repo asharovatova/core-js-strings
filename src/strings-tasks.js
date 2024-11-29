d4488a0db209e650b0ecc50151bc40c49d5d9034
asharovatova <co.rin@mail.ru>
2024-11-30 02:41:40 +0300

    feat: implement removeFirstOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -154,8 +154,9 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  if (str.indexOf(value) !== -1) return str.replace(value, '');
+  return str;
 }
 
 /**
